2681aa945726f552a29bc38ff39fd0177acdefc4
Jake Gutierrez <theborgdude@gmail.com>
2015-01-13 12:46:49 -0600

    Changed flashing colors

--- Sp00ky Site/javasc.js
+++ Sp00ky Site/javasc.js
@@ -1,8 +1,8 @@
 function blink() {
-  document.getElementById("flashing").style.color = "red";
+  document.getElementById("flashing").style.color = "gray";
   setTimeout("green()", 100);
 }
 function green() {
-  document.getElementById("flashing").style.color = "green";
+  document.getElementById("flashing").style.color = "black";
   setTimeout("blink()", 100);
 }
